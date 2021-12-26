3ae046a8ecb9fc54864af9c860c49e203bd7bf34
Fostix <accretiondiskthat@gmail.com>
2021-12-26 18:25:28 +0500

    maked number shift in first if this number even

--- hi/Program.cs
+++ hi/Program.cs
@@ -1,28 +1,49 @@
 ﻿// Написать программу, которая из имеющегося массива целых чисел формирует массив из четных чисел. 
 // Первоначальный массив можно ввести с клавиатуры, либо сгенерировать случайным образом. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
-
-int[] array = new int[5];
-
+int arrayLengthz = 5;
+int[] array = new int[arrayLengthz];
+int temp;
+int count = 0;
 void FillArray(int[] fill)
 {
     for (int row = 0; row < fill.Length; row++)
     {
-        fill[row] = new Random().Next();
+        fill[row] = new Random().Next(1, 10);
     }
 }
-
-void FormirationArray(int[] form)
-{
-    for (int row = 0; row < form.Length; row++)
-    {
-        
-    }
-}
-
-
-
-
-
 FillArray(array);
-FormirationArray(array);
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i]);
+}
+
+for (int row = 0; row < array.Length; row++)
+{
+    if (0 == array[row] % 2)
+    {
+        temp = array[row];
+        array[row] = 0;
+        for (int howRow = 0; howRow < array.Length; howRow++)
+        {
+            if (0 != array[howRow] % 2 | 0 == array[howRow])
+            {
+                array[howRow] = temp;
+                count++;
+                break;
+            }
+        }
+        Console.Write(temp + " темп");
+    }
+}
+
+
+
+Console.WriteLine();
+
+for (int row = 0; row < array.Length; row++)
+{
+    Console.WriteLine(array[row] + "ар");
+}
+Console.WriteLine(count);
