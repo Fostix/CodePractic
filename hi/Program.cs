b224e3ca2dfc578e555503c5a4d7fc3af2e93af8
Fostix <accretiondiskthat@gmail.com>
2021-12-26 16:14:54 +0500

    maked array in which array entered random numbers

--- hi/Program.cs
+++ hi/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива целых чисел формирует массив из четных чисел. 
+// Первоначальный массив можно ввести с клавиатуры, либо сгенерировать случайным образом. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+int[] array = new int[5];
+
+void FillArray(int[] fill)
+{
+    for (int row = 0; row < fill.Length; row++)
+    {
+        fill[row] = new Random().Next();
+    }
+}
+
+void FormirationArray(int[] form)
+[
+    
+]
+
+
+
+
+
+FillArray(array);
+FormirationArray(array);
