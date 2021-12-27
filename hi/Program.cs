af3d20350f2f9f4621dc421b21bd284f3af6f8c9
Fostix <accretiondiskthat@gmail.com>
2021-12-27 06:13:32 +0500

    start make method

--- hi/Program.cs
+++ hi/Program.cs
@@ -2,9 +2,8 @@
 // Первоначальный массив можно ввести с клавиатуры, либо сгенерировать случайным образом. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 int arrayLengthz = 5;
-int[] array = new int[arrayLengthz];
-int temp;
-int count = 0;
+int[] first = new int[arrayLengthz];
+
 
 
 
@@ -15,51 +14,78 @@ void FillArray(int[] fill)
         fill[row] = new Random().Next(1, 10);
     }
 }
-FillArray(array);
-
-for (int i = 0; i < array.Length; i++)
+FillArray(first);
+for (int i = 0; i < first.Length; i++)
 {
-    Console.Write(array[i]);
+    //Console.Write(first[i]);
 }
 
 
-for (int row = 0; row < array.Length; row++)
+
+(int[], int) ForShift(int[] first)
 {
-    if (0 == array[row] % 2)
+    int temp;
+    int count = 0;
+    for (int row = 0; row < first.Length; row++)
     {
-        temp = array[row];
-        array[row] = 0;
-        for (int howRow = 0; howRow < array.Length; howRow++)
+        if (0 == first[row] % 2)
         {
-            if (0 != array[howRow] % 2 | 0 == array[howRow])
+            temp = first[row];
+            first[row] = 0;
+            for (int howRow = 0; howRow < first.Length; howRow++)
             {
-                array[howRow] = temp;
-                count++;
-                break;
+                if (0 != first[howRow] % 2 | 0 == first[howRow])
+                {
+                    first[howRow] = temp;
+                    count++;
+                    break;
+                }
             }
+            Console.Write(temp + " темп");
         }
-        Console.Write(temp + " темп");
     }
+    return (first, count);
 }
 
-int[] arrayCopy = new int[count];
 
-for (int row = 0; row < arrayCopy.Length; row++)
+(int[], int) lol = ForShift(first);
+int[] array = lol.Item1;
+int count = lol.Item2;
+
+
+for (int i = 0; i < count; i++)
 {
-    arrayCopy[row] = array[row];
+
+    Console.Write(array);
 }
 
-Console.WriteLine();
 
-for (int row = 0; row < array.Length; row++)
-{
-    Console.WriteLine(array[row] + "ар");
-}
-Console.WriteLine(count);
-Console.WriteLine();
+//Console.WriteLine(count);
 
-for (int row = 0; row < arrayCopy.Length; row++)
-{
-    Console.WriteLine(arrayCopy[row] + "ар");
-}
+
+//int[] arrayCopy = new int[count];
+
+
+
+
+
+
+// for (int row = 0; row < arrayCopy.Length; row++)
+// {
+//     arrayCopy[row] = first[row];
+// }
+
+// Console.WriteLine();
+
+// for (int row = 0; row < first.Length; row++)
+// {
+//     Console.WriteLine(first[row] + "ар");
+// }
+// //Console.WriteLine(count);
+// Console.WriteLine();
+
+// for (int row = 0; row < arrayCopy.Length; row++)
+// {
+//     Console.WriteLine(arrayCopy[row] + "ар");
+// }
 
